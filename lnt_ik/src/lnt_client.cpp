11dce5e0cc17c24c517bea263018df1363ca661b
gokul <astrogok@gmail.com>
2018-01-06 17:45:47 +0530

    Added a server for individual_joint_control mode with safety check functionality

--- lnt_ik/src/lnt_client.cpp
+++ lnt_ik/src/lnt_client.cpp
@@ -1,24 +1,62 @@
 #include "ros/ros.h"
 #include "lnt_ik/lnt_ik.h"
 #include <lnt_packet/lnt_packet.h>
+#include <stdio.h>
 
 
-//Global variable for extracting the values from packet data
-int packet;
-float x,y,z,alpha,beta,gama;
-float r,theta;
-int mode;
-//variable for storing joint angles(radians) 
-double position,multiple_position[6]; 
-//variable for storing the joint number
-int joint_num;
 
-void packet_data_Callback(const lnt_packet::lnt_packet::ConstPtr& packet_data)
+
+class lnt_data
+{
+ private:
+	//variable for storing safety limits for each joints
+	double max[6] = {3.14,3.14,0,2.09,1.57,3.14};
+double min[6] = {-3.14,0,-3.14,-2.09,-1.57,-3.14};
+
+ public:
+	//Global variable for extracting the values from packet data
+	int packet=0;
+	float x,y,z,alpha,beta,gama;
+	float r,theta;
+        float values[6]; 
+         
+	int mode;
+	ros::NodeHandle n;
+	//variable for storing joint angles(radians) 
+	double position=50;
+	double multiple_position[6]; 
+	//variable for storing the joint number
+	int joint_num=0;
+
+	void packet_data_Callback(const lnt_packet::lnt_packet::ConstPtr& packet_data);
+
+        bool safety_check(int joint, double position);
+
+        bool calling_service();
+};
+
+
+//Safety limit check function
+bool lnt_data::safety_check(int joint, double position)
+{
+  if ((position>=min[joint]) && (position<=max[joint]))
+    return true;
+  else
+    return false;
+
+}
+
+void lnt_data::packet_data_Callback(const lnt_packet::lnt_packet::ConstPtr& packet_data)
 {
    
   //Assigning the packet data to corresponding global variables
+  ROS_INFO("packet=%d",packet);
   packet = packet_data->packet_code;
   mode = packet_data->eff_mode;
+   for(int i=0;i<6;i++)
+	 {
+		 values[i] = packet_data->values[i];
+	 }
   if(packet == 1)
   {
 	  x = packet_data->values[0];
@@ -36,8 +74,15 @@ void packet_data_Callback(const lnt_packet::lnt_packet::ConstPtr& packet_data)
   }
   else if(packet == 5)
   {
+	  
 	  joint_num = packet_data->values[0];
 	  position = packet_data->values[1];
+	  //Radians Conversion
+	  position = (position *3.14)/180;
+	  std::cout<<"position"<<position;
+	  
+          
+
   }
   else if(packet == 6)
   {
@@ -49,47 +94,59 @@ void packet_data_Callback(const lnt_packet::lnt_packet::ConstPtr& packet_data)
  
 }
 
+bool lnt_data::calling_service()
+{
+	 
+	 //ROS_INFO("packet=%d",packet);
+     ros::NodeHandle n;
+     ROS_INFO("calling service");
+     
+	 if(lnt_data::safety_check(joint_num,position))
+	  {
+	   //creating client for the server joint_space_control_individual
+       ros::ServiceClient lnt_client = n.serviceClient<lnt_ik::lnt_ik>("joint_space_control_individual");
+	   
+	   lnt_ik::lnt_ik srv;
+	   for(int i=0;i<6;i++)
+	 {
+		 srv.request.values[i] = values[i];
+	 }
+		   
+           lnt_client.call(srv);
+           ros::Duration(5).sleep();
+           
+   }
+  
+}
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "lnt_manipulator_client");
   ros::NodeHandle node_handle;
-  ros::NodeHandle n;
 
-  //creating client for lnt
-  ros::ServiceClient lnt_client = n.serviceClient<lnt_ik::lnt_ik>("lnt_server");
 
+  
+
+ 
   //Subscriber for packet data
-  ros::Subscriber sub1 = node_handle.subscribe("lnt_packet_data", 1000, packet_data_Callback);
+  lnt_data l1;
+  ros::Subscriber sub1 = node_handle.subscribe("lnt_packet_data", 1000, &lnt_data::packet_data_Callback, &l1);
 
+  
+  //ROS_INFO("calling service");
+  l1.calling_service();
+ 
   //spinner thread for subscribers
-  ros::AsyncSpinner spinner(3);
+  ros::AsyncSpinner spinner(4);
   spinner.start();
+  ros::waitForShutdown();
+  
    
- //creating the srv object for request
- lnt_ik::lnt_ik srv;
-  
-   while(ros::ok())
-  { 
-	if(packet == 5)
-  {
-	if (lnt_client.call(srv))
-  {
-    ROS_INFO("success");
-  }
-  else
-  {
-    ROS_ERROR("Failed");
-    return 1;
-  }
-
   return 0;
-}
 
-  
-  }	
-  
-}
+ 	
+  }
 
 
 
--- lnt_ik/src/lnt_server.cpp
+++ lnt_ik/src/lnt_server.cpp
@@ -1,29 +1,153 @@
 #include "ros/ros.h"
 #include "lnt_ik/lnt_ik.h"
 
+//Moveit header files
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+//Visulaization tools
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+//For mathematical calculation and conversion
+#include <math.h>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+#include <cmath>
+#include <tf/transform_datatypes.h>
+
 class lnt_control
 {
+ private:
+           
+	//Variable for storing the planning group which we want
+	static constexpr const char* PLANNING_GROUP = "manipulator";  //static cannot be used for string data member and so using char* 
+	
+	//Pointer object for MoveGroupInterface class
+	moveit::planning_interface::MoveGroupInterface *move_group;
+	
+	//Pointer variable for storing state(position) of joint model group	
+	const robot_state::JointModelGroup *joint_model_group;
+	
+	//Create an object for storing the curret state of the robot
+	moveit::core::RobotStatePtr current_state;  
+	
+	//Temporary variable for copying the joint group positions
+	std::vector<double> joint_group_positions;  
+	
+	//variable for storing safety limits for each joints
+	double max[6] = {3.14,3.14,0,2.09,1.57,3.14};
+	double min[6] = {-3.14,0,-3.14,-2.09,-1.57,-3.14};
+	
+	//Angle converted to radians and stored
+	float position;
+  	
+
  public:
 	bool individual_joint_control(lnt_ik::lnt_ik::Request& req,lnt_ik::lnt_ik::Response& res);
+	
+	bool multiple_joint_control(lnt_ik::lnt_ik::Request& req,lnt_ik::lnt_ik::Response& res);
+	
+	bool safety_check(int joint, double position);
+	
+	float Deg_to_Rad(float angle);
+	
+	lnt_control();
 }; 
+
+//Class constructor
+lnt_control::lnt_control()
+{
+	//Invoke the movegroup interface for the planning group which we want
+	  move_group = new moveit::planning_interface::MoveGroupInterface(PLANNING_GROUP);
+ }
+ 
+ //Safety limit check function
+bool lnt_control::safety_check(int joint, double position)
+{
+  if ((position>=min[joint]) && (position<=max[joint]))
+    return true;
+  else
+    return false;
+}
+
+float lnt_control::Deg_to_Rad(float angle)
+{
+	 angle = (angle *3.14)/180;
+	 ROS_INFO("Angle=%f",angle);
+	 return angle;
+}
+
+
 
 bool lnt_control::individual_joint_control(lnt_ik::lnt_ik::Request& req,lnt_ik::lnt_ik::Response& res)
 {
-	res.result =1;
-	ROS_INFO("  sending back response: [%d]", res.result);
-	return true;
+  position = lnt_control::Deg_to_Rad(req.values[1]);
+  if(lnt_control::safety_check(req.values[0],position))
+  {
+	 //Store the current state information(position/accleration/velocity) from the movegroup member function
+      current_state = move_group->getCurrentState();
+      
+      //Get the current position of the joints of the corresponding planning group
+      joint_model_group = current_state->getJointModelGroup(PLANNING_GROUP);
+  
+      //Copy the positions to another variable for modification.
+      current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+      // Modify the joint state accordingly
+      joint_group_positions[req.values[0]] = position;  // radians
+      move_group->setJointValueTarget(joint_group_positions);
+      
+      //Create a plan object(local member) 
+      moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+      bool success = move_group->plan(my_plan);
+
+      //Execute the plan
+      move_group->move(); 
+
+      res.result = success;
+
+      ROS_INFO("Sending back response: [%d]", res.result);
+      return true;
   }
+  else
+  {
+	  ROS_INFO("Exceeding joint limits");
+      return false;
+  }
+}
+
+bool lnt_control::multiple_joint_control(lnt_ik::lnt_ik::Request& req,lnt_ik::lnt_ik::Response& res)
+{
+	
+	
+	
+}
+
 
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "lnt_manipulator_server");
         ros::NodeHandle n;
-	
+
 	//Creating an object of the class
 	lnt_control arm;
-	ros::ServiceServer service =  n.advertiseService("lnt_server", &lnt_control::individual_joint_control, &arm);
-	ros::spin();
+	ros::ServiceServer service1 =  n.advertiseService("joint_space_control_individual", &lnt_control::individual_joint_control, &arm);
+	ros::ServiceServer service2 =  n.advertiseService("joint_space_control_multiple", &lnt_control::multiple_joint_control, &arm);
+	ros::AsyncSpinner spinner(3);
+  	spinner.start();
+	ros::waitForShutdown();
 	return 0;
 }
 
